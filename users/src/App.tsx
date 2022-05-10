0b7140b6b9e9ee01b0f741edb8bfb5083d717394
gondor159 <lacls159@gmail.com>
2022-05-10 17:05:54 +0900

    컴포넌트 추가

--- users/src/App.tsx
+++ users/src/App.tsx
@@ -1,6 +1,8 @@
 import React, {useState, useEffect} from 'react';
 import {IUser} from './IUser';
 import {getDataPromise} from './getDataPromise';
+import { Card } from './Card';
+import './App.css';
 
 const App: React.FC = () => {
   const limit = 1;
@@ -17,9 +19,9 @@ const App: React.FC = () => {
   useEffect(onClick, []);
   
   return (
-    <div className='App'>
-      <p><button onClick={onClick}>more data...</button></p>
-      <p>{JSON.stringify(users)}</p>
+    <div className='App'>{
+      users.map((user: IUser, key: number) => (<Card click={onClick} user={user} key={key.toString()} />))
+    }
     </div>
   );
 }
--- users/src/IUser.ts
+++ users/src/IUser.ts
@@ -1,7 +1,7 @@
 export interface IUser {
   _id: string;
   name: string;
-  emial: string;
+  email: string;
   sentence: string;
   profession: string;
   birthday: Date;
