425cb584a75c99e09fa1a2248adbbc6915c83e98
gondor159 <lacls159@gmail.com>
2022-05-10 14:00:21 +0900

    문서 삭제

---
+++ src/test/delete-test.ts
@@ -0,0 +1,27 @@
+import { connect } from '../mongodb/connect';
+
+const deleteTest = async () => {
+  let connection;
+
+  try {
+    connection = await connect();
+    const db = await connection.db('BigdataBatch');
+    const personsCollection = db.collection('persons');
+    await personsCollection.insertMany([
+      { name: 'Jack' },
+      { name: 'Tom' },
+      { name: 'Jane' },
+    ]);
+
+    let result = await personsCollection.deleteOne({ name: 'Tom' });
+    console.log(result);
+    result = await personsCollection.deleteMany({});
+    console.log(result);
+  } catch (e) {
+    console.log(e.message);
+  } finally {
+    connection.close();
+  }
+};
+
+deleteTest();
