6626500509bbe4720f5693cc114a300b0ab35b25
gondor159 <lacls159@gmail.com>
2022-05-10 15:21:10 +0900

    라우팅 기능 추가

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,8 @@ const app = express(),
 
 app
   .get('/', (req, res) => res.json({ message: 'Hello world!' }))
+  .get('/users/:skip/:limit', (req, res) => {
+    const { skip, limit } = req.params;
+    res.json({ skip, limit });
+  })
   .listen(port, () => console.log(`http://localhost:${port} started...`));
