829fe5263d193cd2cf7626e167eadf5318162800
gondor159 <lacls159@gmail.com>
2022-05-10 16:40:43 +0900

    useState 추가

--- users/src/App.tsx
+++ users/src/App.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {IUser} from './IUser';
 import {getDataPromise} from './getDataPromise';
 
 const App: React.FC = () => {
-  getDataPromise((users: IUser[]) => console.log('users', users))(0, 1);
+  const [users, setUsers] = useState<IUser[]>([]);
+  getDataPromise((receivedUsers: IUser[]) => {
+    setUsers([...users, ...receivedUsers])
+  })(0, 1);
+
   return (
-    <div className='App'>please open console window!</div>
+    <div className='App'>{JSON.stringify(users)}</div>
   );
 }
 
