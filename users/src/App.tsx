7ed3460711d8f3585a8f3451bf6e8f08ebc6ddc1
gondor159 <lacls159@gmail.com>
2022-05-10 16:47:38 +0900

    useEffect 추가

--- users/src/App.tsx
+++ users/src/App.tsx
@@ -1,13 +1,16 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {IUser} from './IUser';
 import {getDataPromise} from './getDataPromise';
 
 const App: React.FC = () => {
   const [users, setUsers] = useState<IUser[]>([]);
-  getDataPromise((receivedUsers: IUser[]) => {
-    setUsers([...users, ...receivedUsers])
-  })(0, 1);
-
+  
+  useEffect(() => {
+    getDataPromise((receivedUsers: IUser[]) => {
+      setUsers([...users, ...receivedUsers])
+    })(0, 1);
+  }, []);
+  
   return (
     <div className='App'>{JSON.stringify(users)}</div>
   );
