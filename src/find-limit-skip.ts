1e67484f41aa42323c55d57e281ab4fb158e6e04
gondor159 <lacls159@gmail.com>
2022-05-10 14:39:51 +0900

    limit와 skip

---
+++ src/find-limit-skip.ts
@@ -0,0 +1,30 @@
+import { connect } from './mongodb/connect';
+import { IFake } from './fake';
+
+const findLimitskip = async () => {
+  let connection, cursor;
+  try {
+    connection = await connect();
+    const db = await connection.db('BigdataBatch');
+    const usersCollection = db.collection('users');
+
+    let cursor = await usersCollection
+      .find({})
+      .sort({ birthday: -1, name: 1 })
+      .skip(100)
+      .limit(5);
+    let result = await cursor.toArray();
+    console.log(
+      result.map((user: IFake) => ({
+        name: user.name,
+        birthday: user.birthday,
+      })),
+    );
+  } catch (e) {
+    console.log(e.message);
+  } finally {
+    connection.close();
+  }
+};
+
+findLimitskip();
